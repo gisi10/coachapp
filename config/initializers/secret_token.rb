9ea00581c87e59e46c29a723d921e60b9f0bea6e
Goetz <hallo.goetz@gmx.de>
2012-12-08 12:55:55 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coachapp::Application.config.secret_token = '2cc93db198ea226ecd8e6e5dd696fe56d67ff8423958455dff05576c33281c2cadd9de3c6d6af4a0c8b63408b887b31c01e066eeb912e81bfb23ee1e7229017d'
