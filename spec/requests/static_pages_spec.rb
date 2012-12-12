4ff48d3c996f295734fb272886fc1eff182752c3
Goetz <hallo.goetz@gmx.de>
2012-12-12 21:17:29 +0100

    routes und layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,18 +6,18 @@ describe "Static pages" do
 
 	describe "Home page" do
 
-		it "should have the content 'Home'" do
-			visit '/static_pages/home'
-			page.should have_selector('h1', :text => 'Home')
+		it "should have the content 'Willkommen bei der Coachapp'" do
+			visit root_path
+			page.should have_selector('h1', :text => 'Wilkommen bei der Coachapp')
 		end
 
 		it "should have the base title" do
-		  visit '/static_pages/home'
+		  visit root_path
 		  page.should have_selector('title', :text => "Coachapp")
 		end
 
 		it "should not have a custom page title" do
-			visit "/static_pages/home"
+			visit root_path
 			page.should_not have_selector('title', :text => '| Home')		  
 		end
 	end
@@ -25,17 +25,17 @@ describe "Static pages" do
 	describe "help page" do
 
 		it "should have the content 'Hilfe" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('h1', :text => 'Hilfe')
 		end
 
 		it "should have the base title" do
-		  visit '/static_pages/help'
+		  visit help_path
 		  page.should have_selector('title', :text => "Coachapp")
 		end
 
 		it "should not have a custom page title" do
-			visit "/static_pages/help"
+			visit help_path
 			page.should_not have_selector('title', :text => '| Home')		  
 		end
 	end
@@ -43,17 +43,17 @@ describe "Static pages" do
 	describe "About page" do
 
 		it "should have the content 'Impressum'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('h1', :text => 'Impressum')
 		end
 
 		it "should have the base title" do
-		  visit '/static_pages/about'
+		  visit about_path
 		  page.should have_selector('title', :text => "Coachapp")
 		end
 
 		it "should not have a custom page title" do
-			visit "/static_pages/about"
+			visit about_path
 			page.should_not have_selector('title', :text => '| Home')		  
 		end
 	end
@@ -61,17 +61,17 @@ describe "Static pages" do
 	describe "Contact page" do
 
 		it "should have the content 'Kontakt'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			page.should have_selector('h1', :text => 'Kontakt')
 		end
 
 		it "should have the base title" do
-		  visit '/static_pages/contact'
+		  visit contact_path
 		  page.should have_selector('title', :text => "Coachapp")
 		end
 
 		it "should not have a custom page title" do
-			visit "/static_pages/contact"
+			visit contact_path
 			page.should_not have_selector('title', :text => '| Home')		  
 		end
 	end
