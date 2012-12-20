ebc019164c1a5591faf14dccbbd4e3bdb0ee7aad
Goetz <hallo.goetz@gmx.de>
2012-12-20 20:36:06 +0100

    Make basic user  model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,39 +2,48 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	let(:base_title){"Coachapp"}
 	subject {page}
+
+	shared_examples_for "all static pages" do
+		it { should have_selector('h1', text: heading) }
+		it { should have_selector('title', text: full_title(page_title)) }
+	end
+
 
 	describe "Home page" do
 		before { visit root_path}
+		let(:page_title) { '' }
+		let(:heading) { "Coachapp" }
 
-		it {should have_selector('h1', text: 'Coachapp')}
-		it {should have_selector('title', text: full_title(''))}	
+		it_should_behave_like "all static pages"
 		it { should_not have_selector 'title', text: '| Home' }	  
 	
 	end
 
 	describe "help page" do
 		before { visit help_path}
-
-		it {should have_selector('h1', text: 'Hilfe')}
-		it {should have_selector('title', text: full_title(''))}	
+		let(:page_title) { '' }
+		let(:heading) { 'Hilfe' }
+		
+		it_should_behave_like "all static pages"
 		it { should_not have_selector 'title', text: '| Hilfe' }	 
 	end
 
 	describe "About page" do
 		before {visit about_path}
-
-		it {should have_selector('h1', text: 'Impressum')}
-		it {should have_selector('title', text: full_title(''))}	
+		let(:page_title) {''}
+		let(:heading) {  'Impressum'}
+		
+		it_should_behave_like "all static pages"
 		it { should_not have_selector 'title', text: '| Impressum' }	 
 	end
 
 	describe "Contact page" do
 		before {visit contact_path}
-
-		it {should have_selector('h1', text: 'Kontakt')}
-		it {should have_selector('title', text: full_title(''))}	
+		let(:page_title) { '' }
+		let(:heading) { 'Kontakt' }
+		
+		it_should_behave_like "all static pages"	
 		it { should_not have_selector 'title', text: '| Kontakt' }	 
 	end
 end
