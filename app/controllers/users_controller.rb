878a10e7bb1a0a61c2ad3d56c04e01e3b756ee7b
Goetz <hallo.goetz@gmx.de>
2012-12-26 12:30:46 +0100

    zur sicherheit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,16 @@ class UsersController < ApplicationController
   end	
 
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])  	
+  	if @user.save
+  		flash[:success] = "Willkommen bei der Coachapp"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name "TittiTester"
+		email "TT@tstr.net"
+		password "foobar"
+		password_confirmation "foobar"
+	end	
+end
